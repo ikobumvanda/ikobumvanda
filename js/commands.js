b532cc94d1c611af2c144cc7766d8736f8ceb404
ikobumvanda <125932215+ikobumvanda@users.noreply.github.com>
2023-02-22 22:50:26 +0100

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -1,12 +1,4 @@
-/* social = [
-  "<br>",
-  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
-  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
-  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
-  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
-  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
-  "<br>"
-]; */
+var email = "mailto:ikobumvanda@gmail.com";
 
 help = [
   "<br>",
@@ -36,20 +28,40 @@ banner = [
   "   👅    ",
   "  👉👈  ",
   "  🦵🦵   ",
-  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  "<span class=\"color2\" style=\"font-weight: bold;\">You must solve all tasks as quickly as possible if you wish to lessen your further suffering!</span>",
+  "<span class=\"color2\">For the list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
 ];
 
 questions = [
   [],
   ["<span class=\"color2\">Which year is it?</span>"],
+  ["<span class=\"color2\">What is your spirit animal?</span>"],
+  [
+    "<span class=\"color2\">What is the name of a multinational news channel and website headquartered in Atlanta, Georgia, U.S.</span>",
+    "<span class=\"color2\">It was founded in 1980 by American media proprietor Ted Turner and Reese Schonfeld as a 24-hour </span>",
+    "<span class=\"color2\">cable news channel, and is presently owned by the Manhattan-based media conglomerate Warner Bros. Discovery,</span>",
+    "<span class=\"color2\">and was the first television channel to provide 24-hour news coverage and the first all-news television channel in the US?"],
+  [
+    'Shave off a patch of hair anywhere on your body before 28th Feb 2023 at 8:04',
+    'and send a picture of that spot and the hair to: <a href=\"mailto:ikobumvanda@gmail.com\">ikobumvanda@gmail.com</a>'
+  ],
 ]
 
 answers = [
-  "", 
-  "1984",
+  [""], 
+  ["1984"],
+  ["wolf", "vuk", "🐺"],
+  ["cnn"],
+  ["yes"],
 ]
 
 hints = [
   ["<span class=\"color2\">First activate a task with <span class=\"command\">'task'</span></span>"],
   ["<span class=\"color2\">George Orwell's most famous novel!</span>"],
+  ["<span class=\"color2\">There's two (2) of them inside you!</span>"],
+  [
+    "<span class=\"color2\">The news network has an acronym that is also used in machine learning for a type of algorithm that uses </span>",  
+    "<span class=\"color2\">the operation of convolution and is most often used in computer vision!</span>"
+  ],
+  ["<span class=\"color2\">حتى الله لا يستطيع أن ينقذك الآن</span>"],
 ]
